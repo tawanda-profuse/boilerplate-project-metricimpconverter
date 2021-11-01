dcbb48f227179d47b69ad93f73c6c09ba0e48eec
Tawanda Andrew <xrthemc@gmail.com>
2021-11-01 10:34:04 +0200

    Update api.js
    Completing the necessary routes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,30 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
+  app.route("/api/convert").get((req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
 
+    const validNum = typeof initNum !== "object";
+    const validUnit = typeof initUnit !== "object";
+    if (!validNum && !validUnit) return res.json({ error: "invalid number and unit" });
+    if (!validNum) return res.json({ error: "invalid number" });
+    if (!validUnit) return res.json({ error: "invalid unit" });
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    });
+  });
 };
